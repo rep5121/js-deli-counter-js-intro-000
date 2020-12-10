78e1c5a2c9c448c59fcba32204d6c7a1ce80e365
bold-mixer-0681 <renee.pujol@gmail.com>
2020-12-10 03:55:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  console.log(katzDeliLine);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  }
+
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length > 0){
+      let curName = katzDeliLine.shift();
+      return `Currently serving ${curName}.`
+      }
+
+  else{
+      return "There is nobody waiting to be served!"
+      }
+}
+
+function currentLine(deliLine){
+  if (deliLine.length > 0){
+    for( let i = 0; i < deliLine.length; i++){
+      let count = 0;
+      count = count + 1;
+      return `"The line is currently: ${count}. ${deliLine[i]},...`
+      }
+
+  }
+
+  else{
+  return "The line is currently empty."
+  }
+
+}
